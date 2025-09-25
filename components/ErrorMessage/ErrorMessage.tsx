762c4d3d05d3350fb98dfd848a3fda663e8217d6
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-09-25 17:11:35 +0200

    copi components

---
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import css from "./ErrorMessage.module.css";
+
+export default function ErrorMessage() {
+  return (
+    <div className={css.wrapperError}>
+      <p className={css.erroeMessage}>
+        Oops! Something went wrong. Try again.{" "}
+      </p>
+    </div>
+  );
+}
